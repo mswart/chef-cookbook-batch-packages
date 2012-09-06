82fbfced87f47becdee728f2ba34735144fc3363
James Kyle <james@jameskyle.org>
2012-09-06 11:51:46 -0700

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "AT&T Foundry"
+maintainer_email "james.kyle@att.com"
+license          "All rights reserved"
+description      "Installs/Configures batch-packages"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: batch-packages
+# Recipe:: default
+#
+# Copyright 2012, AT&T Foundry
+#
+# All rights reserved 
+
+# get list of available package collections
+bags = data_bag("batch-packages")
+
+# get list of roles applied to this node
+roles = node['roles'].dup
+# add the 'base' role that all nodes receive
+roles.insert(0, "base")
+
+# get the intersection of roles and available package collections
+needed = roles & bags
+
+# install packages for each collection
+needed.each do |role|
+  pkglist = search("batch-packages", "id:#{role}").first
+  if pkglist then
+    pkglist['packages'].each do |p, v|
+      Chef::Log.info "Installing package #{p}..."
+      if v then
+        package p do
+          version v
+          action :install
+        end
+      else
+        package p do
+          action :install
+        end
+      end
+    end
+  end
+end
